ee38de61715ec7ec7b33f34c2bf7ace74e713463
heyimmatt <mrmcfarland@gmail.com>
2020-07-22 09:22:45 -0700

    Add starting code in app and setup home route

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
+from flask import Flask, redirect, request, render_template, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
 from boggle import Boggle
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '1579'
+
+debug = DebugToolbarExtension(app)
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
 boggle_game = Boggle()
+
+@app.route('/')
+def home():
+    return '<h1>Boggle Time</h1>'
